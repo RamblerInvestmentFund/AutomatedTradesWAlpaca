e933d31f1f119183ce450c3c2404431d2fc5415e
Collin Jones <cjones28@luc.edu>
2021-03-02 18:06:57 -0600

    runs but does not work

--- alpha_vantage_data.py
+++ alpha_vantage_data.py
@@ -1,8 +1,9 @@
-from alpha_vantage.techindicators import *
+from alpha_vantage.techindicators import TechIndicators
+from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt 
 import config
 import pandas
-import profile
+import alpaca_profile
 
 
 class AlphaVantageData:
@@ -10,6 +11,20 @@ class AlphaVantageData:
     def __init__(self, symbol):
         self.sym = symbol
         self.ti = TechIndicators(key=config.ALPHA_VANTAGE_KEY, output_format='pandas')
-        self.profile = profile.Profile()
-        self.sma = self.ti.get_sma(symbol=self.sym, interval='60min', time_period=20)
+        self.ts = TimeSeries(key=config.ALPHA_VANTAGE_KEY, output_format='pandas')
+        self.ap = alpaca_profile.Profile()
+        
+    #def tradesma(self):
+        #sma, ti_metadata = self.ti.get_sma(symbol=self.sym, interval='1min', time_period=60, series_type='close')
+        #data_ts, metadata_ts = self.ts.get_intraday(symbol=self.sym,interval='1min', outputsize='full')
+
+    def tradeBB(self):
+        bb, bb_metadata = self.ti.get_bbands(symbol=self.sym, interval='60min', time_period=60)
+        data_ts, metadata_ts = self.ts.get_intraday(symbol=self.sym, interval='60min', outputsize='full')
+        #real vars taken from anthony
+        real_middle = bb['2021-03-01 16:00:00']['Real Middle Band'].item()
+        real_close = data_ts['2021-03-01 16:00:00']['4. close'].item()
+
+        if(real_middle < real_close):  
+            self.ap.send_basic_order( self.sym, 1, 'buy', 'bracket', 'gtc')
         
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 #Pull technical data from AlphaVantage and place trades through Alpaca
 
-from alpha_vantage.techindicators import *
-import matplotlib.pyplot as plt 
-import pandas
+#from alpha_vantage.techindicators import *
+#import matplotlib.pyplot as plt 
+#import pandas
+import alpaca_profile as ap
+import alpha_vantage_data as av
+import config
 
-ti = TechIndicators(key='6IRQ2UGMYR33M3DC', output_format='pandas')
-data , metadata = ti.get_sma(symbol='TSLA', interval='60min', time_period=20)
-data.plot()
-plt.title('simple moving average time series for tesla stock (60 min)')
-plt.show()
+algo = av.AlphaVantageData('TSLA')
+profile = ap.Profile()
+algo.tradeBB()
